bf49911e888363fd52bf294c997a01f6c018ada9
BishwasXD <110834432+BishwasXD@users.noreply.github.com>
2024-01-18 20:58:51 +0545

    Update models.py

--- account/models.py
+++ account/models.py
@@ -5,10 +5,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
     def create_user(self, email, name, tc, password=None, password2 = None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
+      
         if not email:
             raise ValueError("Users must have an email address")
 
@@ -23,10 +20,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, tc, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
+       
         user = self.create_user(
             email = email,
             password = password,
